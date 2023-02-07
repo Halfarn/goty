70e30e3cd8b570d25f53ac33d0a5986285ddd6f7
Halfarn <halfarn@gmail.com>
2023-02-07 12:20:13 +0000

    first push

---
+++ app.js
@@ -0,0 +1,7 @@
+document.querySelector("btn")
+document.getElementById("btn").addEventListener("click", myFunction)
+function myFunction() {
+    var check=document.getElementById("check").value == "Elden Ring";
+    if (check){document.getElementById("answer").innerHTML = "You were correct! The answer was Elden Ring"}
+    if(!check){document.getElementById("answer").innerHTML = "You were incorrect. Try again!"}
+}
